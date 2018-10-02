b1e37def13693d511587d43361c7aa7239db33f1
Matheus Vitório <mvfc@users.noreply.github.com>
2018-10-02 16:45:53 -0300

    Create CreateFolderWithPermissions.ps1

---
+++ CreateFolderWithPermissions.ps1
@@ -0,0 +1,12 @@
+param([string]$username, [string]$path, [string]$name)
+$instance = instance
+$db = reportserver
+$query = 'exec sp_make_new_dir @DIRNAME = "'+$name+'", @DIRPATH = "'+$path+'", @USERNAME = "'+$username+'"'
+
+
+
+Invoke-SqlCmd -ServerInstance $instance -Database $db -Query $query
+
+$pathofnewdir = $path+'/'+$name
+
+Grant-AccessOnCatalogItem -Identity $username -RoleName 'Content Manager' -Path $pathofnewdir -ReportServerUri 'http://localhost/reportserver'
